0139f8c2018c6ef9ebad58be9f06de68e01e9b17
didita91 <didif.91@gmail.com>
2015-03-06 22:27:22 -0300

    it1

--- gdt_project/apps/views.py
+++ gdt_project/apps/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+def login_redirect(request):
+    """Redirige de /accounts/login a /login."""
+    return HttpResponseRedirect('/login')
--- gdt_project/gdt_project/urls.py
+++ gdt_project/gdt_project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'apps.views.login_redirect', name='login_redirect'),
 )
