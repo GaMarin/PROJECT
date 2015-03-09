0f1d3898bfdae63c9c7fd7a8c7df8f7f97202c19
didita91 <didif.91@gmail.com>
2015-03-09 16:22:49 -0300

    it1

--- gdt_project/gdt_project/urls.py
+++ gdt_project/gdt_project/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.contrib.auth import authenticate, login
 
 urlpatterns = patterns('',
                        # Examples:
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
 
                        # url(r'^admin/', include(admin.site.urls)),
-                       url(r'^login/$', 'django.contrib.auth.views.login'),
+                       url('^', include('django.contrib.auth.urls')),
 )
