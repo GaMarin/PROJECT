1a53e73cc0bd642ce6a923ec9637c60e4f246ba3
Teresita <correo@correo.com>
2015-03-10 08:23:48 -0300

    Test. prueba vista1

--- gdt_project/apps/tests.py
+++ gdt_project/apps/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+#from .models import Entry
+
+
 
 # Create your tests here.
+class ViewTests(TestCase):
+    def test_feed_url(self):
+        response = self.client.get('/feed/')
+        self.assertIn("xml", response['Content-Type'])
+        raise notimplementedError#error a proposito
